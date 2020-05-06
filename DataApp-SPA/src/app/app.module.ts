43926285e15f21835fc27d951c65689a038f9092
John <johnandbev2000@yahoo.co.uk>
2020-05-06 06:41:30 +0100

    End of SEction 10

--- DataApp-SPA/src/app/app.module.ts
+++ DataApp-SPA/src/app/app.module.ts
@@ -21,6 +21,9 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function myTokenGetter() {
   return localStorage.getItem('token');
@@ -44,6 +47,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
     MessagesComponent,
     ListsComponent,
     MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     FormsModule,
@@ -65,8 +69,10 @@ export class CustomHammerConfig extends HammerGestureConfig {
   providers: [
     AuthService,
     ErrorInterceptorProvider,
+    PreventUnsavedChanges,
     MemberDetailResolver,
     MemberListResolver,
+    MemberEditResolver,
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig     }
   ],
   bootstrap: [AppComponent],
--- DataApp-SPA/src/app/route.ts
+++ DataApp-SPA/src/app/route.ts
@@ -7,6 +7,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 // first match wins....
 export const appRoutes: Routes = [
@@ -22,7 +25,9 @@ export const appRoutes: Routes = [
          canActivate: [AuthGuard],
          children: [
             { path: 'members', component: MemberListComponent, resolve: {currentUsers: MemberListResolver}},
-            { path: 'members/:id', component: MemberDetailComponent, resolve: {currentUser: MemberDetailResolver}},
+                { path: 'members/:id', component: MemberDetailComponent, resolve: {currentUser: MemberDetailResolver}},
+                { path: 'member/edit', component: MemberEditComponent,
+                 resolve: {currentUser: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent }
          ]
