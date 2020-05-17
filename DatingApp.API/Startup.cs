28d9b7a18ea71520bed92dd84640daaef6be4d75
John <johnandbev2000@yahoo.co.uk>
2020-05-17 07:36:18 +0100

    End Section 13 - Action Filter last active

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -63,6 +63,8 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+
+                services.AddScoped<LogUserActivity>();// our custom action filter
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
